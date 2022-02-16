26f0296d2e38a77d5e546d8fc7a0c7443681e0d9
Eric Lofgren <Eric.Lofgren@wsu.edu>
2022-02-16 00:41:09 -0800

    Update survival.R
    Some code cleanup

--- Week 4 - Survival Analysis/survival.R
+++ Week 4 - Survival Analysis/survival.R
@@ -1,8 +1,11 @@
 library(readr)
 library(survival)
+library(flexsurv)
+library(survminer)
 
 # Read in the data set
 leu <- read_csv("survival.csv")
+leu$treated2 <- as.factor(leu$treated)
 
 # Create the "survival object" - this will be used in *many* future stages
 trialtimes <- Surv(leu$time, leu$relapse)
@@ -12,24 +15,32 @@ fit_raw <- survfit(trialtimes ~ 1)
 plot(fit_raw)
 
 # Fit different KM curves by treatment and controls
-fit_treatment <- survfit(trialtimes ~ leu$treated)
+fit_treatment <- survfit(Surv(time, relapse) ~ treated, data=leu)
 
-plot(fit_treatment,conf.int=FALSE,col=c("red","blue"),mark.time=TRUE,xlab="Weeks",ylab="Proportion Recurring")
-legend("topright", c("Treated","Untreated"), lwd=3, col=c("blue","red"),
-       lty=c(1,1), pch=c(NA,NA), bty='n')
+ggsurvplot(fit_treatment)
+ggsurvplot(fit_treatment,risk.table=TRUE)
+ggsurvplot(fit_treatment,pval=TRUE)
+ggsurvplot(fit_treatment,conf.int=TRUE)
 
 trial_p <- survdiff(trialtimes ~ leu$treated)
 
 # Fit a Cox PH model
-coxmodel <- coxph(trialtimes ~ leu$treated)
+coxmodel <- coxph(Surv(time, relapse) ~ treated, data=leu)
 summary(coxmodel)
 
 # Test of the proportional hazard assumption
 prophaz <- cox.zph(coxmodel)
 prophaz
-plot(prophaz)
+ggcoxzph(prophaz)
+
+ggcoxdiagnostics(coxmodel,type="schoenfeld")
 
 # Parametric Survival models
-summary(survreg(trialtimes ~ leu$treated, dist="exponential"))
+expo <- flexsurvreg(Surv(time, relapse) ~ treated2, data=leu, dist="exponential")
+expo
+plot(expo)
+ggflexsurvplot(expo,risk.table=TRUE)
 
-summary(survreg(trialtimes ~ leu$treated, dist="weibull"))
+wei <- flexsurvreg(Surv(time, relapse) ~ treated2, data=leu, dist="weibull")
+wei
+ggflexsurvplot(wei)
